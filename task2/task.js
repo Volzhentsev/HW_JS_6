1f7a6e95c3552925b43cfec21b92b0cddda2797f
Volzhentsev Sergey <volzhentsev@bk.ru>
2022-08-21 19:32:56 +0300

    task2 modified

--- task2/task.js
+++ task2/task.js
@@ -2,8 +2,18 @@ function menu() {
     const menuBar = Array.from(document.querySelectorAll(".menu__link"));
     for (let i = 0; i < menuBar.length; i++) {
         menuBar[i].onclick = () => { 
-            menuBar[i].nextElementSibling.className='menu menu_sub menu_active';
-            return false 
+            if ((menuBar[i].nextElementSibling != null)) {
+                if (menuBar[i].nextElementSibling.className == "menu menu_sub menu_active") {
+                    menuBar[i].nextElementSibling.className = "menu menu_sub";
+                    return false
+                }
+                const closeMenu = document.getElementsByClassName("menu menu_sub menu_active");
+                if (closeMenu.length > 0) {
+                    closeMenu[0].className = "menu menu_sub";
+                }
+                menuBar[i].nextElementSibling.className = "menu menu_sub menu_active";
+                return false 
+            }               
         }
     }     
 }
